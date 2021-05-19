24dbb6b8e72167ebbf119a5739abc6b661a4e989
Lisa <bear.bessie@gmail.com>
2021-05-19 23:46:10 +0300

    2 дз

---
+++ src/appline/Variables.java
@@ -0,0 +1,10 @@
+package appline;
+
+public class Variables {
+    public static double dVar = 12345.6789;
+    public static byte bVar = 127;
+    public static void main(String[] args) {
+        System.out.println("double variable = "+ dVar);
+        System.out.println("byte variable = "+ bVar);
+    }
+}
