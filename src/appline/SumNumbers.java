d2c14f23d42657bc0d87f44e24e25dede885b947
Lisa <bear.bessie@gmail.com>
2021-05-22 06:37:22 +0300

    8 дз

---
+++ src/appline/SumNumbers.java
@@ -0,0 +1,27 @@
+package appline;
+
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите положительное целое число");
+        int n = scan.nextInt();
+
+        if (n < 1) {
+            System.out.println("Ошибка. Введено отрицательное число число");
+        }
+        else {
+            int sum = 0;
+            // Вариант для суммы, не включающей число n
+            for (int i=1; i < n; i+=2){
+            /* // Вариант для суммы, влючающей число n
+            for (int i=1; i <= n; i+=2){
+             */
+                sum += i;
+            }
+            System.out.println("Сумма нечетных чисел от 1 до " + n + " = " + sum);
+        }
+    }
+}
